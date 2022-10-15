6e574a24e9d0a07e1f3f92ca432246972039882c
Shanvi2021 <100283082+Shanvi2021@users.noreply.github.com>
2022-10-15 14:18:24 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,31 @@
+variable "azurem_security_group_name" {
+  description = "nsg_name"
+  type        = list(string)
+}
+
+variable "azurerm_resource_group_nsg_location" {
+  type        = string
+  description = "nsg location"
+}
+
+variable "azurerm_resource_group_nsg_name" {
+  type        = string
+  description = "Resource group name"
+ }
+
+
+variable "azurem_custom_nsg_rules" {
+  type = list(object({
+    name                       = list(string)
+    priority                   = list(number)
+    direction                  = list(string)
+    access                     = list(string)
+    protocol                   = list(string)
+    source_port_range          = list(string)
+    destination_port_range     = list(string)
+    source_address_prefix      = list(string)
+    destination_address_prefix = list(string)
+  }))
+  description = "security_rule"
+}
+
